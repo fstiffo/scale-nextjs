4195c1c1bd0e7c75960178403375ca0d73fb4111
Francesco Stiffoni <fstiffo@sgajo.com>
2022-09-10 10:43:01 +0000

    Test of datepicker and daisyui

---
+++ pages/index copy.js
@@ -0,0 +1,50 @@
+import Head from 'next/head';
+import JournalEntry from '../components/JournalEntry';
+import prisma from '../lib/prisma';
+
+export default function Home({ JournalEntries }) {
+  return (
+    <div>
+      <Head>
+        <title>PlanetScale Next.js Quickstart</title>
+        <meta name="description" content="PlanetScale Quickstart for Next.js" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+      </Head>
+
+      <main className="p-10 mx-auto max-w-4xl">
+        <h1 className="text-6xl font-bold mb-4 text-center">Next.js Starter</h1>
+        <p className="mb-20 text-xl text-center">
+          🔥 Shop from the hottest items in the world 🔥
+        </p>
+        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center  gap-4">
+          {JournalEntries.map((JournalEntry) => (
+            <JournalEntry JournalEntry={JournalEntry} key={JournalEntry.id} />
+          ))}
+        </div>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
+
+export async function getStaticProps(context) {
+  console.log("Prisma: ", prisma);
+  const data = await prisma.JournalEntry.findMany({
+    include: {
+      account: true,
+    },
+  });
+
+  //convert decimal value to string to pass through as json
+  const JournalEntries = data.map((JournalEntry) => ({
+    ...JournalEntry,
+    debit: JournalEntry.debit.toString(),
+    credit: JournalEntry.credit.toString(),
+    date: JournalEntry.date.toString()
+  }));
+  return {
+    props: { JournalEntries },
+  };
+}
--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,15 @@
-import Head from 'next/head';
-import JournalEntry from '../components/JournalEntry';
-import prisma from '../lib/prisma';
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import { forwardRef, useEffect, useState } from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import { format } from 'date-fns'
 
-export default function Home({ JournalEntries }) {
+export default function Home() {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div>
-      <Head>
-        <title>PlanetScale Next.js Quickstart</title>
-        <meta name="description" content="PlanetScale Quickstart for Next.js" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
-      </Head>
-
-      <main className="p-10 mx-auto max-w-4xl">
-        <h1 className="text-6xl font-bold mb-4 text-center">Next.js Starter</h1>
-        <p className="mb-20 text-xl text-center">
-          🔥 Shop from the hottest items in the world 🔥
-        </p>
-        <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 justify-items-center  gap-4">
-          {JournalEntries.map((JournalEntry) => (
-            <JournalEntry JournalEntry={JournalEntry} key={JournalEntry.id} />
-          ))}
-        </div>
-      </main>
-
-      <footer></footer>
+      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
     </div>
   );
-}
-
-export async function getStaticProps(context) {
-  console.log("Prisma: ", prisma);
-  const data = await prisma.JournalEntry.findMany({
-    include: {
-      account: true,
-    },
-  });
-
-  //convert decimal value to string to pass through as json
-  const JournalEntries = data.map((JournalEntry) => ({
-    ...JournalEntry,
-    debit: JournalEntry.debit.toString(),
-    credit: JournalEntry.credit.toString(),
-    date: JournalEntry.date.toString()
-  }));
-  return {
-    props: { JournalEntries },
-  };
 }
