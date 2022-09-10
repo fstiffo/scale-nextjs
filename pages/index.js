2162ea8412f42e3148546989915159056901d8e8
Francesco Stiffoni <fstiffo@sgajo.com>
2022-09-10 10:45:19 +0000

    Fix className

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div>
       <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-      <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
+      <button className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">Button</button>
     </div>
   );
 }
