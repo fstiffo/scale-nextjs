aef859780d44ebf414a7298a6830d9df85a132c1
Francesco Stiffoni <fstiffo@sgajo.com>
2022-09-12 13:37:44 +0200

    Update db schema

--- prisma/fill-monthly-payment.js
+++ prisma/fill-monthly-payment.js
@@ -8,15 +8,24 @@ const load = async () => {
 
         const apartments = await prisma.apartment.findMany({ select: { id: true } });
         apartments.forEach(async ({ id }) => {
-            const monthlyDues = await prisma.journalEntry.findMany({
+            // Not booked yet dues payments of apartment `id`
+            const duesPayments = await prisma.journalEntry.findMany({
                 orderBy: { date: "desc" },
-                where: { apartment_id: id },
+                where: {
+                    AND: [
+                        { apartment_id: id },
+                        { booked: false }]
+                },
                 select: { date: true, credit: true, }
             });
-            console.log(monthlyDues);
+            console.log(duesPayments);
             const lastMonthlyPayment = await prisma.monthlyPayment.findFirst({
                 orderBy: [{ year: "desc" }, { month: "desc" }],
                 where: { apartment_id: id }
+            });
+            const { monthly_dues } = await prisma.configuration.findFirst({
+                select: { monthly_dues: true },
+                orderBy: { valid_from: "desc" }
             });
 
 
